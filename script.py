62a322c64457b2bf8bbb71d8e6a488ffa1080d0c
William Joseph Mark <wjosephmark@gmail.com>
2022-05-12 21:59:48 -0600

    my first commit

---
+++ script.py
@@ -0,0 +1,20 @@
+import boto3
+from botocore.exceptions import ClientError
+import os
+
+def upload_file(file_name, bucket, object_name=None):
+  s3_client = boto3.client('s3')
+
+  if object_name is None:
+    object_name = os.path.basename(file_name)
+
+  try:
+    response = s3_client.upload_file(file_name, bucket, object_name)
+  except ClientError as e:
+    print(e)
+    return 'Failed to upload file'
+  return 'File uploaded successfully'
+
+response = upload_file('test.txt', 'wjm-test-bucket')
+
+print(response)
